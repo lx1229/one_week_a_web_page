be59dc39d1e193888d19312538edeb58eb1563d3
LiuXin <xin.liu1@thoughtworks.com>
2023-11-09 23:16:27 +0800

    feat: submit code

--- week-1/src/pages/main/Main.jsx
+++ week-1/src/pages/main/Main.jsx
@@ -1,7 +1,29 @@
+import "./main.style.scss";
+import Astronauts from "../../img/astronauts.png";
 export const Main = () => {
   return (
-    <>
-      <h1>Test</h1>
-    </>
+    <div className="main-content">
+      <div className="main-1st-part">
+        <div className="title">
+          Russia’s Youngest-ever Astronauts Blast Off to Space Station
+        </div>
+        <div className="desc">
+          On Thursday, three Russian astronauts lifted off on the Moscow-17
+          spacecraft from the Sputnik 1 Satellite Launch Center deep in the Gobi
+          Desert, heading for the Mir space station for a six-month stay.
+        </div>
+        <div className="footer">
+          <span>Space and Universe</span>
+          <span>|</span>
+          <div>Daniel Albarta</div>
+          <span>|</span>
+          <span>October 22, 2023</span>
+        </div>
+      </div>
+      <div className="main-2nd-part">
+        <img src={Astronauts} alt="astronauts from Russian" />
+      </div>
+      <div></div>
+    </div>
   );
 };
